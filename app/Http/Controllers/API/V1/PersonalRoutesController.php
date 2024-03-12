d5851bc3c10cbd2c192e709ea7cb8e5f6ad6d409
thera1DDD <tagir566666@gmail.com>
2024-03-12 12:57:41 +0300

    perosnal route adding api fixed

--- app/Http/Controllers/API/V1/PersonalRoutesController.php
+++ app/Http/Controllers/API/V1/PersonalRoutesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Routing\StoreRequest;
+use App\Http\Resources\API\MainDisplayResource\PersonalRouteResource;
 use App\Models\Routing;
 use Illuminate\Http\JsonResponse;
 
@@ -15,8 +16,8 @@ class PersonalRoutesController extends Controller
     }
 
     public function addRoute(StoreRequest $request): JsonResponse
-    {   $data =  $request->validated();
-        $route = Routing::create($data);
-        return response()->json(['result' => $route]);
+    {
+        $route = Routing::create($request->validated());
+        return response()->json(['result' => new PersonalRouteResource($route)]);
     }
 }
