5ac0650d057f6fe908536eab0b461c7d491afdb7
thera1DDD <tagir566666@gmail.com>
2024-03-14 17:53:42 +0300

    migratioin fix

--- database/migrations/2024_03_09_120614_change_users_fields.php
+++ database/migrations/2024_03_09_120614_change_users_fields.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('verification_code');
+            $table->string('verification_code')->nullable();
         });
     }
 
