49143ccab775a92de2b106d6bbb98099068cd590
thera1DDD <tagir566666@gmail.com>
2024-03-16 21:29:33 +0300

    fix phone_number repeat

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -40,20 +40,20 @@ class AuthService extends Controller
     public function sendSMS($data, $code): JsonResponse
     {
         try {
-                // Отправляем SMS через API
-                $response = Http::post(env('SMS_API') .'/sms/send', [
-                    'user' => env('SMS_LOGIN'),
-                    'pass' => env('SMS_PASSWORD'),
-                    'to' => $data['phone_number'],
-                    'txt' => 'Ваш код для авторизации: '.$code,
-                    'from' => 'Perevozki',
-                ]);
+//                // Отправляем SMS через API
+//                $response = Http::post(env('SMS_API') .'/sms/send', [
+//                    'user' => env('SMS_LOGIN'),
+//                    'pass' => env('SMS_PASSWORD'),
+//                    'to' => $data['phone_number'],
+//                    'txt' => 'Ваш код для авторизации: '.$code,
+//                    'from' => 'Perevozki',
+//                ]);
                 // Добавление пользователя
                 $userData = $this->postUser($data['phone_number'],$code);
                 return response()->json([
                     'success' => true,
                     'data' => [
-                        'green_sms_data' => $response,
+//                        'green_sms_data' => $response,
                         'code' => $code,
                         'user' => $userData,
                     ],
@@ -67,7 +67,7 @@ class AuthService extends Controller
     public function postUser($phone, $code)
     {
         $user = User::where('phone_number',$phone)->first();
-        if($user){
+        if(isset($user)){
             $user->verification_code = $code;
             $user->save();
             return $user;
