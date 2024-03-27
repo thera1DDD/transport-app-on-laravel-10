b400c3d16bbe52737091aa8c306d8b361ea565a7
thera1DDD <tagir566666@gmail.com>
2024-03-27 16:07:37 +0300

    test

--- app/Http/Controllers/API/V1/MainDisplayController.php
+++ app/Http/Controllers/API/V1/MainDisplayController.php
@@ -19,8 +19,8 @@ class MainDisplayController extends Controller
         $this->routingService = $routingService;
     }
     public function getRoutes(): JsonResponse {
-        $carrierRoutes = (new Routing)->filterByRouteType('carrier')->paginate(4);
-        $senderRoutes = (new Routing)->filterByRouteType('sender')->paginate(4);
+        $carrierRoutes = (new Routing)->filterByRouteType('carrier')->with('user')->paginate(4);
+        $senderRoutes = (new Routing)->filterByRouteType('sender')->with('user')->paginate(4);
         return response()->json(['carrier'=>$carrierRoutes,'sender'=>$senderRoutes]);
     }
 
