8c5cee9817eb139e837a3fcf6fa0369392e5d7d6
thera1DDD <tagir566666@gmail.com>
2024-03-31 18:30:43 +0300

    paginate remove

--- app/Http/Controllers/API/V1/PersonalRoutesController.php
+++ app/Http/Controllers/API/V1/PersonalRoutesController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Routing\StoreRequest;
 use App\Http\Resources\API\MainDisplayResource\PersonalRouteResource;
 use App\Models\Routing;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 
 class PersonalRoutesController extends Controller
 {
-    public function getMyRoutes(int $users_id)
+    public function getMyRoutes(int $users_id): Collection|array
     {
-        return Routing::with('user')->where('owners_id',$users_id)->paginate(4);
+        return Routing::with('user')->where('owners_id',$users_id)->get();
     }
 
     public function addRoute(StoreRequest $request): JsonResponse
